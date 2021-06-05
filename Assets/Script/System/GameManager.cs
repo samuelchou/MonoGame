513bd39fda41212f5f1b4504e6cac47f6853595e
samuelchou <samuelchou@hotmail.com.tw>
2021-06-05 23:22:51 +0800

    feat(system/script): GameManager 移動並提供新功能 QuitGame.

---
+++ Assets/Script/System/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Script.System
+{
+    public class GameManager : MonoBehaviour
+    {
+        public void QuitGame()
+        {
+            Debug.Log("Quit Game");
+            Application.Quit();
+        }
+    }
+}
