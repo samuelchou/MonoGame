0412ccf04c063091f64563fe26c6f7d58e3a1e0d
samuelchou <samuelchou@hotmail.com.tw>
2021-06-05 22:38:11 +0800

    feat(system): 初步架設 LevelManager 與 ProgressAdder.

---
+++ Assets/Script/LevelObject/ProgressAdder.cs
@@ -0,0 +1,17 @@
+﻿using Script.System;
+using UnityEngine;
+
+namespace Script.LevelObject
+{
+    public class ProgressAdder : MonoBehaviour
+    {
+        public float addProgress = 1.0f;
+
+        private void OnMouseDown()
+        {
+            Debug.Log("clicked and add progress " + addProgress);
+            GameObject.FindWithTag("System").GetComponent<LevelManager>().AddProgress(addProgress);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/System/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Script.System
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public float progress = 0;
+
+        private void LateUpdate()
+        {
+            if (progress >= 100)
+            {
+                Debug.Log("Finished level.");
+                SceneManager.LoadSceneAsync(Constants.LevelScene.MainScene);
+            }
+        }
+
+        public void AddProgress(float add)
+        {
+            progress += add;
+        }
+    }
+}
