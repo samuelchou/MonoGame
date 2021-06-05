8ead59398e16627363ac8fdf6a5e75dbea4ae042
samuelchou <samuelchou@hotmail.com.tw>
2021-06-05 22:37:18 +0800

    feat(main-menu): 增加主選單的物件，使其更具識別性。

--- Assets/Script/Constants.cs
+++ Assets/Script/Constants.cs
@@ -4,6 +4,7 @@
     {
         public static class LevelScene
         {
+            public const string MainScene = "Main Menu";
             public const string Level1 = "Level 1";
         }
     }
