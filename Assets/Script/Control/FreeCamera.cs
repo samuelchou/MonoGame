95b65aa7c928b5edec38106680ad5a798530194a
samuelchou <samuelchou@hotmail.com.tw>
2021-06-12 22:16:57 +0800

    feat(control): 新增 FreeCamera, 為未來自由視角做準備。

---
+++ Assets/Script/Control/FreeCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Script.Control {
+    public class FreeCamera : MonoBehaviour {
+        public float cursorSensitivity = 0.025f;
+
+        public bool cursorToggleAllowed = true;
+        public KeyCode cursorToggleButton = KeyCode.Escape;
+
+        private bool _isCursorLocked = true;
+
+        private void Update() {
+            if (cursorToggleAllowed)
+                if (Input.GetKeyDown(cursorToggleButton)) {
+                    _isCursorLocked = !_isCursorLocked;
+                    Cursor.visible = !_isCursorLocked;
+                    Cursor.lockState = _isCursorLocked ? CursorLockMode.Locked : CursorLockMode.None;
+                }
+
+            if (_isCursorLocked) {
+                var eulerAngles = transform.eulerAngles;
+                eulerAngles.x += -Input.GetAxis("Mouse Y") * 359f * cursorSensitivity;
+                eulerAngles.y += Input.GetAxis("Mouse X") * 359f * cursorSensitivity;
+
+                //"overhead" prevention
+                if (eulerAngles.x > 90 && eulerAngles.x < 180) eulerAngles.x = 90;
+                if (eulerAngles.x < 270 && eulerAngles.x > 180) eulerAngles.x = 270;
+
+                //apply
+                transform.eulerAngles = eulerAngles;
+            }
+        }
+
+        private void OnEnable() {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
