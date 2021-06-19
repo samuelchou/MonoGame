f7c2c08c730102adc69928da488a22c373614483
samuelchou <samuelchou@hotmail.com.tw>
2021-06-19 21:39:39 +0800

    feat(menu): 針對 Menu 相關介面提供 FreeCursor, 以解放因關卡而被綁住的游標。

---
+++ Assets/Script/Control/FreeCursor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Script.Control {
+    public class FreeCursor : MonoBehaviour {
+        private void OnEnable() {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
