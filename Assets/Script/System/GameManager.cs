0e702b15520473602f1f9263c49c6425502c640c
samuelchou <samuelchou@hotmail.com.tw>
2021-06-19 20:51:32 +0800

    feat(game-manager): 改寫 GameManager, 提供「載入關卡」功能。

--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace Script.System
-{
-    public class GameManager : MonoBehaviour
-    {
-        public void QuitGame()
-        {
+namespace Script.System {
+    public class GameManager : MonoBehaviour {
+        public void ChangeScene(string sceneName) {
+            SceneManager.LoadSceneAsync(sceneName);
+        }
+
+        public void QuitGame() {
             Debug.Log("Quit Game");
             Application.Quit();
         }
