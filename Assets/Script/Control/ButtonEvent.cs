ef6a36416f845417245fcdedaf8eb5edf9baa601
samuelchou <samuelchou@hotmail.com.tw>
2021-06-19 22:07:59 +0800

    feat(controller): 新增「按鈕事件」單元腳本。

---
+++ Assets/Script/Control/ButtonEvent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/**
+ * On Button pressed, trigger event.
+ */
+public class ButtonEvent : MonoBehaviour {
+    public KeyCode inputKey = KeyCode.Tab;
+    public UnityEvent triggerEvent;
+
+    private void FixedUpdate() {
+        if (Input.GetKeyDown(inputKey)) triggerEvent.Invoke();
+    }
+}
