090c56ff0654ef3becdd3470a69564336322865d
samuelchou <samuelchou@hotmail.com.tw>
2021-06-26 21:02:54 +0800

    feat(object): 現在 ObjectRevealer 會嘗試「揭露」對應對象的所有資訊。
    包含： Renderer (形狀), 所有 Behaviour 子系列腳本。

--- Assets/Script/LevelObject/ObjectRevealer.cs
+++ Assets/Script/LevelObject/ObjectRevealer.cs
@@ -10,6 +10,10 @@ public class ObjectRevealer : MonoBehaviour {
     private void OnTriggerEnter(Collider other) {
         var targetObj = other.gameObject;
         Debug.Log("detected obj entered: " + targetObj.name);
-        if (targetObj.CompareTag(revealTag)) targetObj.SetActive(true);
+        if (!targetObj.CompareTag(revealTag)) return;
+        targetObj.SetActive(true);
+        targetObj.GetComponent<Renderer>().enabled = true;
+        var behaviours = targetObj.GetComponents<Behaviour>();
+        foreach (var behaviour in behaviours) behaviour.enabled = true;
     }
 }
