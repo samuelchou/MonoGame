48551c992b5f990a9596df75e42fb141de612d0a
samuelchou <samuelchou@hotmail.com.tw>
2021-06-26 20:47:08 +0800

    feat(Object): 完成 ObjectRevealer 的初步設計。

---
+++ Assets/Script/LevelObject/ObjectRevealer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+/**
+ * Object revealer, used to reveal objects when they entered trigger.
+ */
+[RequireComponent(typeof(Collider))]
+public class ObjectRevealer : MonoBehaviour {
+    public string revealTag = "";
+
+    private void OnTriggerEnter(Collider other) {
+        var targetObj = other.gameObject;
+        Debug.Log("detected obj entered: " + targetObj.name);
+        if (targetObj.CompareTag(revealTag)) targetObj.SetActive(true);
+    }
+}
