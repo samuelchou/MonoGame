dd2775eb15fe138e79d2b39a20461a587837451d
samuelchou <samuelchou@hotmail.com.tw>
2021-10-02 22:33:13 +0800

    feat(pause-menu/system): 新增 Pause Menu 場景，並提供開關功能。

--- Assets/Script/System/Constants.cs
+++ Assets/Script/System/Constants.cs
@@ -6,6 +6,7 @@
         {
             public const string MainScene = "Main Menu";
             public const string Level1 = "Level 1";
+            public const string PauseMenu = "PauseMenu";
         }
     }
 }
--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -2,10 +2,56 @@
 using UnityEngine.SceneManagement;
 
 namespace Script.System {
+
     public class GameManager : MonoBehaviour {
         public void ChangeScene(string sceneName) {
             SceneManager.LoadSceneAsync(sceneName);
         }
+
+        #region Pause Menu
+
+        private bool isPaused = false;
+        private bool hasloadedPauseMenu = false;
+        public KeyCode pauseKey = KeyCode.Escape;
+
+        private void Update() {
+            if (Input.GetKeyDown(pauseKey)) {
+                TogglePause();
+            }
+        }
+
+        public void TogglePause() {
+            if (!isPaused)
+            {
+                OpenPauseMenu();
+                isPaused = true;
+            }
+            else
+            {
+                ClosePauseMenu();
+                isPaused = false;
+            }
+
+        }
+
+        private void OpenPauseMenu() {
+            Debug.Log("Open pause menu.");
+            SceneManager.LoadSceneAsync(Constants.LevelScene.PauseMenu, LoadSceneMode.Additive);
+            hasloadedPauseMenu = true;
+        }
+
+        public void ClosePauseMenu() {
+            Debug.Log("Closing pause menu...");
+            if (!hasloadedPauseMenu)
+            {
+                Debug.LogError("Pause Menu not loaded! Won't do anything.");
+                return;
+            }
+            SceneManager.UnloadSceneAsync(Constants.LevelScene.PauseMenu);
+            hasloadedPauseMenu = false;
+        }
+
+        # endregion
 
         public void RestartScene() {
             SceneManager.LoadScene(
